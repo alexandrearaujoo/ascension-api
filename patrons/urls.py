ecec95a06b1757054cc8f2d3912cdd07164fd435
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-11 15:49:10 -0300

    fix: separate list and create view

--- patrons/urls.py
+++ patrons/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('patron/register/', views.CreatePatronView.as_view()),
-    path('patron/', views.ListPatronView.as_view()),
+    path('patrons/', views.ListPatronView.as_view()),
     path('patron/login/', views.LoginPatronView.as_view())
 ]
