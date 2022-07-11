90788be8e1825b91819f4ddaf1eab26281814731
aggierosa <agnes.rosa@hotmail.com>
2022-07-11 11:20:05 -0300

    feat: models

--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    username = models.CharField(max_length=50, unique=True, null=False)
+    password = models.CharField()
+    level = models.IntegerField(max_length=3, default=0)
+    experience = models.IntegerField(default=0)
+    gold = models.IntegerField(default=0)
+
+    patron = models.ForeignKey(
+        "patrons.Patron", on_delete=models.DO_NOTHING, related_name="characters"
+    )
