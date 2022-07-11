fee5a1016da4da8e363d6090f31d92e7548b3be2
aggierosa <agnes.rosa@hotmail.com>
2022-07-11 11:19:24 -0300

    feat: serializer

---
+++ characters/serializer.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from .models import Character
+
+from Patrons.serializer import PatronSerializer
+
+
+class CharacterSerializer(serializers.ModelSerializer):
+    patron = PatronSerializer(read_only=True)
+
+    class Meta:
+        model = Character
+        fields = "__all__"
+        read_only_fields = ["gold", "experience", "level"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+
+class CharacterManagementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        fields = "__all__"
+
+
+class CharacterLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
