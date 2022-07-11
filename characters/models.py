9140ef2e3d71c7e4e31d45e7b3c605a1a64dcfbc
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-11 11:32:38 -0300

    fix: removing max_length in integer field and add auth user model in settings

--- characters/models.py
+++ characters/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Character(models.Model):
     username = models.CharField(max_length=50, unique=True, null=False)
     password = models.CharField()
-    level = models.IntegerField(max_length=3, default=0)
+    level = models.IntegerField(default=0)
     experience = models.IntegerField(default=0)
     gold = models.IntegerField(default=0)
 
