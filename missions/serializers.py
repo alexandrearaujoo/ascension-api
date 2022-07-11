65ecf94ce7a04d700e3a8865225d376201693808
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-11 13:33:08 -0300

    feat: mission serializer done

---
+++ missions/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from django.core.validators import MinValueValidator
+
+from patrons.serializer import PatronSerializer
+
+from .models import Missions
+
+class MissionSerializer(serializers.ModelSerializer):
+    created_by = PatronSerializer(read_only=True)
+
+    class Meta:
+        model = Missions
+        fields = '__all__'
+        extra_kwargs = {
+            'experience': {'validators': [MinValueValidator(0)]},
+            'level_required': {'validators': [MinValueValidator(0)]},
+            'gold': {'validators': [MinValueValidator(0)]},
+            'xp': {'validators': [MinValueValidator(0)]}
+        }
