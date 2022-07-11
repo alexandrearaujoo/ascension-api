32f8176efe184934c0e1fd6ecf8e4ef87324ed42
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-11 09:46:55 -0300

    feat: mission model done

--- missions/models.py
+++ missions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Missions(models.Model):
+    description = models.TextField(null=False)
+    name = models.CharField(max_length=50, null=False)
+    experience = models.IntegerField(null=False)
+    level_required = models.IntegerField()
+    gold = models.IntegerField()
+    xp = models.IntegerField()
+
+    created_by = models.ForeignKey(
+        'patrons.Patron', 
+        on_delete=models.CASCADE, 
+        related_name='mission'
+    )
