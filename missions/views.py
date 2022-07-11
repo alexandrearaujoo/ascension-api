3753bba85259971dcb66b37a45d5056ace3947be
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-11 14:48:44 -0300

    feat: views and permission missions done

--- missions/views.py
+++ missions/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+from .permissions import MissionsCustomPermissions
+
+from missions.serializers import MissionSerializer
+
+from .models import Missions
+
+class ListCreateMission(generics.ListCreateAPIView):
+    queryset = Missions.objects.all()
+    serializer_class = MissionSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [MissionsCustomPermissions]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
