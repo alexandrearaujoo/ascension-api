654daf77c4e41d034ae074334969aec492186b57
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-07-11 10:08:12 -0300

    feat: Formatting code and create artisan model

--- artisans/apps.py
+++ artisans/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ArtisansConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'artisans'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "artisans"
--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
+    path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
