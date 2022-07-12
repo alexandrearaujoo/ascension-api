b90eea710a2d7104307befc09937ab603dfb5d31
aggierosa <agnes.rosa@hotmail.com>
2022-07-12 16:05:15 -0300

    feat/fix: character remodel

--- characters/models.py
+++ characters/models.py
@@ -2,18 +2,24 @@ from django.db import models
 
 
 class Character(models.Model):
+
     username = models.CharField(max_length=50, unique=True, null=False)
     password = models.CharField(max_length=255)
     level = models.IntegerField(default=0)
     experience = models.IntegerField(default=0)
     gold = models.IntegerField(default=0)
+    health_points = models.IntegerField(default=100)
 
-    patron = models.ForeignKey(
-        "patrons.Patron",
-        on_delete=models.DO_NOTHING,
-        related_name="characters",
+    strength = models.IntegerField(default=0)
+    intellect = models.IntegerField(default=0)
+    agility = models.IntegerField(default=0)
+
+    vocation = models.ForeignKey(
+        "vocations.Vocation", on_delete=models.CASCADE, related_name="characters"
     )
 
-    missions = models.ManyToManyField(
-        "missions.Missions", related_name="characters"
+    account = models.ForeignKey(
+        "accounts.Account", on_delete=models.DO_NOTHING, related_name="characters"
     )
+
+    missions = models.ManyToManyField("missions.Missions", related_name="characters")
--- characters/urls.py
+++ characters/urls.py
@@ -3,10 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("accounts/", views.ListCreateUserView.as_view()),
+    path("accounts/characters/", views.ListCreateUserView.as_view()),
     path(
-        "accounts/<int:character_id>/",
+        "accounts/characters/<int:character_id>/",
         views.RetrieveUpdateDeleteCharacterView.as_view(),
     ),
-    path("accounts/login/", views.LoginCharacterView.as_view()),
 ]
