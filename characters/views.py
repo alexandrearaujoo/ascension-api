e16952c9b348d14a4c038a422429d6480c5e4cc2
aggierosa <agnes.rosa@hotmail.com>
2022-07-12 09:33:55 -0300

    fix: permission classes character view

--- characters/views.py
+++ characters/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView, Response, status
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from .permissions import IsAdmin, IsAccountOwnerOrReadOnly, isAccountOwner
+from .serializer import CharacterLoginSerializer, CharacterSerializer
+from .models import Character
 
-# Create your views here.
+
+class ListCreateUserView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdmin | isAccountOwner]
+
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
+
+
+class RetrieveUpdateDeleteCharacterView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [isAccountOwner]
+
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
+
+
+class LoginCharacterView(APIView):
+    def post(self, request):
+        serializer = CharacterLoginSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        character = authenticate(
+            username=serializer.validated_data["username"],
+            password=serializer.validated_data["password"],
+        )
+
+        if character:
+            token, _ = Token.objects.get_or_create(user=character)
+
+            return Response({"token": token.key})
+
+        return Response(
+            {"message": "Invalid credentials"}, status.HTTP_401_UNAUTHORIZED
+        )
