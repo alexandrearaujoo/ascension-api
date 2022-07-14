9c8bc3bf122f62147b65c2b9d2f3fdd5fd6f6626
NehamaMandelbaum <nehama1836@gmail.com>
2022-07-13 21:37:40 -0300

    feature: starting characters tests

---
+++ characters/tests/test_views.py
@@ -0,0 +1,10 @@
+from rest_framework.test import APITestCase
+
+
+class TestCharacterView(APITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        ...
+
+    def test_characters(self):
+        ...
