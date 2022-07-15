ad7bd02054c8eca27b90faaf3eb98423f33dc799
Alexandre Araujo <alexandre01393@gmail.com>
2022-07-15 17:23:39 -0300

    update: changing messages of mission completed

--- characters/views.py
+++ characters/views.py
@@ -75,9 +75,9 @@ class PatchMissionCharacterView(APIView):
 
             response = {
                 "message": "Mission succeded",
-                "current level": character_serializer.data["level"],
-                "current gold": character_serializer.data["gold"],
-                "current experience": character_serializer.data["experience"],
+                "current_level": character_serializer.data["level"],
+                "current_gold": character_serializer.data["gold"],
+                "current_experience": character_serializer.data["experience"],
             }
 
             return Response(response)
